171260434650e26a6f40dc36b54cb03dcb0f8ca7
atpopov21 <atpopov21@codingburgas.bg>
2024-01-24 06:34:39 +0200

    Add appllication images to presentation

--- Universal_Vault_Bank_(UVB)/Universal_Vault_Bank_(UVB)/main.cpp
+++ Universal_Vault_Bank_(UVB)/Universal_Vault_Bank_(UVB)/main.cpp
@@ -5,9 +5,9 @@ int main()
 	system("MODE CON COLS=126 LINES=50");	// Set CMD's resolution without resize lock
 	
 	// setting CMD's resolution(cols, lines) to 126x50 and locking resizing; 
-	HWND console = GetConsoleWindow();
+	/*HWND console = GetConsoleWindow();
 
-	/*SMALL_RECT rect = { 0, 0, 126, 50 }; // Adjust the values as needed
+	SMALL_RECT rect = { 0, 0, 126, 50 }; // Adjust the values as needed
 	SetConsoleWindowInfo(console, TRUE, &rect);
 	SetWindowLong(console, GWL_STYLE, GetWindowLong(console, GWL_STYLE) & ~WS_MAXIMIZEBOX & ~WS_SIZEBOX);*/
 
